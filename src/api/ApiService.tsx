ad7635c213af9fcf549b83e0315ec8a13ef34d23
kaka <113968332+Ka213s@users.noreply.github.com>
2024-05-31 05:47:44 +0700

    merge code, funtionc update account, login google

--- src/api/ApiService.tsx
+++ src/api/ApiService.tsx
@@ -1,9 +1,28 @@
 import axios from 'axios';
-
+export interface UserData {
+    fullName: string;
+    email: string;
+    avatar: string;
+    createdAt: string;
+    status: boolean;
+    password: string | null;
+    address: string | null;
+    updateAt: string | null;
+    phonenumber: string | null;
+    walletId: string | null;
+    roleId: number;
+}
 class ApiService {
-    static async getAccounts() {
+    static async saveGoogleUserData(userData: UserData): Promise<void> {
         try {
-            const response = await axios.get('https://66557e453c1d3b602939b8f1.mockapi.io/Account');
+            await axios.post('https://66557e453c1d3b602939b8f1.mockapi.io/Account', userData);
+        } catch (error) {
+            throw new Error('Error saving user data to API');
+        }
+    }
+    static async getAccounts(role: string) {
+        try {
+            const response = await axios.get(`https://66557e453c1d3b602939b8f1.mockapi.io/Account?roleId=${role}`);
             return response.data;
         } catch (error) {
             console.error('Error fetching account data:', error);
@@ -27,6 +46,36 @@ class ApiService {
             return response.data;
         } catch (error) {
             console.error('Error logging in:', error);
+            throw error;
+        }
+    }
+
+    static async getAccountById(roleId: string) {
+        try {
+            const response = await axios.get(`https://66557e453c1d3b602939b8f1.mockapi.io/Account/${roleId}`);
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching account data:', error);
+            throw error;
+        }
+    }
+
+    static async updateAccount(id: string, data: any) {
+        try {
+            const response = await axios.put(`https://66557e453c1d3b602939b8f1.mockapi.io/Account/${id}`, data);
+            return response.data;
+        } catch (error) {
+            console.error('Error updating account:', error);
+            throw error;
+        }
+    }
+
+    static async changePassword(id: string, currentPassword: string, newPassword: string) {
+        try {
+            const response = await axios.put(`https://66557e453c1d3b602939b8f1.mockapi.io/Account/${id}/changePassword`, { currentPassword, newPassword });
+            return response.data;
+        } catch (error) {
+            console.error('Error changing password:', error);
             throw error;
         }
     }
--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,10 +1,11 @@
 import React, { useState, ChangeEvent, FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import ApiService from '../api/ApiService';
+import ApiService, { UserData } from '../api/ApiService';
 import { FaEye, FaEyeSlash, FaGoogle } from 'react-icons/fa';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from '../firebaseConfig';
 import Artwork from '../assets/Artwork.jpg';
 
 interface LoginData {
@@ -35,12 +36,11 @@ const Login: React.FC = () => {
 
         try {
             const response = await ApiService.login(loginData);
-            const account = response.find((account: any) =>
+            const account = response.find((account: UserData) =>
                 account.email === loginData.email &&
                 account.password === loginData.password &&
                 account.status === true
             );
-            console.log(account);
             if (account) {
                 toast.success('Login successful');
                 localStorage.setItem('userData', JSON.stringify(account));
@@ -71,19 +71,70 @@ const Login: React.FC = () => {
     const handleRegisterClick = (): void => {
         navigate('/register');
     };
+
     const togglePasswordVisibility = (): void => {
         setShowPassword(!showPassword);
     };
-    const handleGoogleLogin = (): void => {
-        console.log('Login with Google');
-    };
 
+    const handleGoogleLogin = async (): Promise<void> => {
+        const provider = new GoogleAuthProvider();
+        try {
+            const result = await signInWithPopup(auth, provider);
+            if (result.user) {
+                const user = result.user;
+                const profile = user.providerData[0];
+                const fullName = profile.displayName ?? '';
+                const email = profile.email ?? ''; 
+                const photoURL = profile.photoURL ?? ''; 
+
+                const userData: UserData = {
+                    fullName,
+                    email,
+                    avatar: photoURL,
+                    createdAt: new Date().toISOString(),
+                    status: true,
+                    password: null,
+                    address: null,
+                    updateAt: null,
+                    phonenumber: null,
+                    walletId: null,
+                    roleId: 2 
+                };
+
+                try {
+                    await ApiService.saveGoogleUserData(userData);
+                    toast.success("User logged in successfully!");
+
+                    switch (userData.roleId) {
+                        case 1:
+                            navigate('/adminhome');
+                            break;
+                        case 2:
+                            navigate('/studenthome');
+                            break;
+                        case 3:
+                            navigate('/instructorhome');
+                            break;
+                        default:
+                            break;
+                    }
+                } catch (error) {
+                    toast.error('Error saving user data to API');
+                    console.error('Error saving user data to API:', error);
+                }
+            }
+        } catch (error) {
+            toast.error('Error logging in with Google');
+            console.error('Error logging in with Google:', error);
+        }
+    };    
+    
     return (
         <div className="bg-gray-50 min-h-screen flex items-center justify-center">
             <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-5xl p-5 items-center">
                 <div className='md:w-1/2 px-16'>
                     <h2 className="font-bold text-2xl text-[#6C6EDD]">Login</h2>
-                    <p className='text-base mt-4 text-[#4A4DC3]'>If you already a member,easily log in</p>
+                    <p className='text-base mt-4 text-[#4A4DC3]'>If you already a member, easily log in</p>
 
                     <form onSubmit={handleSubmit} className="flex flex-col gap-4">
                         <input
@@ -142,7 +193,6 @@ const Login: React.FC = () => {
                     <div className='w-full mt-5 border-b border-gray-400'>
                         <button
                             className='text-base py-4 text-[#6C6EDD] text-left cursor-pointer'
-
                         >Forgot your password?</button>
                     </div>
                     <div className='mt-3 text-base flex justify-between items-center'>
@@ -158,11 +208,11 @@ const Login: React.FC = () => {
                 </div>
 
                 <div className='md:block hidden w-1/2'>
-                    <img className='rounded-2xl' src={Artwork} alt="" />
+                <img className='rounded-2xl' src={Artwork} alt="" />
                 </div>
                 <ToastContainer />
             </div>
-        </div >
+        </div>
     );
 };
 
