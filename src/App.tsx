ac649ac5f4f8163c37021f55c5dd3a69105c212f
kaka <113968332+Ka213s@users.noreply.github.com>
2024-06-03 14:34:38 +0700

    Create router

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
-import '../src/styles/App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './page/Login';
-import Register from './page/Register';
-import AdminHome from './page/Admin/AdminHome';
-import StudentProfile from './page/Student/StudentProfile';
-import InstructorProfile from './page/Instructor/InstructorProfile';
-import StudentHome from './page/Student/StudentHome';
-import HomePage from './page/HomePage';
+import React from 'react';
+import './styles/App.css';
+import AppRouters from './routers/AppRouters';
 import Header from './components/Header/Header';
 
-
-function App() {
+const App: React.FC = () => {
   return (
-    <Router>
-      <div className="App">
-      
-        <Routes>
-        <Route path="/" element={<HomePage/>}></Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/adminhome" element={<AdminHome />} />
-          {/* <Route path="/profile-student" element={<StudentProfile />} />
-          <Route path="/profile-instructor" element={<InstructorProfile/>} />
-          <Route path="/studenthome" element={<StudentHome/>} /> */}
-
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+    
+      <AppRouters />
+    </div>
   );
 }
 
