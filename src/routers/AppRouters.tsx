aee2a7804c60385a4f743e07d4b767ef97cb19b7
kaka <113968332+Ka213s@users.noreply.github.com>
2024-06-03 14:38:48 +0700

    Update, note RoleId

--- src/routers/AppRouters.tsx
+++ src/routers/AppRouters.tsx
@@ -11,12 +11,13 @@ import ProtectedRoute from './ProtectedRoute';
 
 const AppRouters: React.FC = () => {
   return (
+    // ADMIN LÀ 1 - STUDENT LÀ 2 - INSTRUCTOR LÀ 3
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/adminhome" element={<ProtectedRoute allowedRoles={['admin']}><AdminHome /></ProtectedRoute>} />
+        <Route path="/adminhome" element={<ProtectedRoute allowedRoles={[1]}><AdminHome /></ProtectedRoute>} />
         <Route path="/profile-student" element={<ProtectedRoute allowedRoles={[2]}><StudentProfile /></ProtectedRoute>} />
         <Route path="/profile-instructor" element={<ProtectedRoute allowedRoles={[3]}><InstructorProfile /></ProtectedRoute>} />
         <Route path="/studenthome" element={<ProtectedRoute allowedRoles={[2]}><StudentHome /></ProtectedRoute>} />
