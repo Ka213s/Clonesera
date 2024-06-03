e52e9b36070df6999dfb85daecf5c2de72ceefe1
Ngocbao22 <107461196+Ngocbao22@users.noreply.github.com>
2024-06-03 17:04:31 +0700

    Update CSS

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,9 +7,17 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ toggleMenu }) => {
   const [showDropdown, setShowDropdown] = useState(false);
+  const [showBellNotification, setShowBellNotification] = useState(false);
+  const [showEmailNotifications, setShowEmailNotifications] = useState(false);
 
   const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
+  };
+  const toggleEmailNotifications = () => {
+    setShowEmailNotifications(!showEmailNotifications); 
+  };
+  const toggleBellNotification = () => {
+    setShowBellNotification(!showBellNotification); 
   };
 
   return (
@@ -38,14 +46,32 @@ const Header: React.FC<HeaderProps> = ({ toggleMenu }) => {
             2
           </div>
         </div>
-        <div className="relative">
-          <FaEnvelope className="text-xl cursor-pointer" />
+        <div className="relative" onClick={toggleEmailNotifications}>
+          <FaEnvelope className="text-xl cursor-pointer" onClick={() => setShowEmailNotifications(true)} /> {/* Thêm sự kiện onClick */}
+          {showEmailNotifications && (
+            <div className="absolute top-0 right-0 mt-10 py-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg">
+              <ul>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Notification 1</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Notification 2</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Notification 3</li>
+              </ul>
+            </div>
+          )}
           <div className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-red-500 rounded-full">
             3
           </div>
         </div>
-        <div className="relative">
-          <FaBell className="text-xl cursor-pointer" />
+        <div className="relative" onClick={toggleBellNotification}>
+          <FaBell className="text-xl cursor-pointer" onClick={() => setShowBellNotification(true)}/>
+          {showBellNotification && (
+            <div className="absolute top-0 right-0 mt-10 py-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg">
+              <ul>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Notification 1</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Notification 2</li>
+                <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Notification 3</li>
+              </ul>
+            </div>
+          )}
           <div className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-red-500 rounded-full">
             3
           </div>
