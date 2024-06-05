1b6366e5a9b026040df02c8b07817079e747d3f0
kaka <113968332+Ka213s@users.noreply.github.com>
2024-06-05 08:50:45 +0700

    update api new

--- src/services/ApiService.tsx
+++ src/services/ApiService.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://66557e453c1d3b602939b8f1.mockapi.io/Account';
+const BASE_URL = 'https://665fbf915425580055b0b389.mockapi.io/Account';
 
 export interface UserData {
     fullName: string;
