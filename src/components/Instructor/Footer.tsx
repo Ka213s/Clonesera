708e37e7431223b2ebb7ec5ae5b8b0145a68de6d
thanhthaoship <nhoxloinhoi2k3@gmail.com>
2024-06-09 00:17:12 +0700

    create ins compo

--- src/components/Instructor/Footer.tsx
+++ src/components/Instructor/Footer.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React from 'react';
+import InstructorLinks from '../../models/FileJson/instructorfooterLinks.json';
 import socialMediaLinksData from '../../models/FileJson/socialMediaLinks.json';
 import { FaFacebook, FaTwitter, FaGoogle, FaInstagram, FaYoutube } from 'react-icons/fa';
 
-
+interface FooterLink {
+  text: string;
+  url: string;
+}
 
 interface SocialMediaLink {
   name: string;
@@ -17,11 +21,36 @@ const socialMediaIcons = {
   FaInstagram: <FaInstagram />,
   FaYoutube: <FaYoutube />
 };
+
 const Footer: React.FC = () => {
+  const columnCount = 3;
+  const itemsPerColumn = Math.ceil(InstructorLinks.insLinks.length / columnCount);
+  const columns = Array.from({ length: columnCount }, (_, index) =>
+    InstructorLinks.insLinks.slice(index * itemsPerColumn, (index + 1) * itemsPerColumn)
+  );
+
   return (
-    <footer className="bg-gray-800 text-white p-4">
-      &copy; 2024 Cursus. All Rights Reserved.
-      <ul className="flex">
+    <footer className="p-4 shadow-md bg-gray-900 bg-opacity-90">
+      <div className="container mx-auto flex justify-between">
+        <div className="flex flex-wrap justify-center w-3/4">
+          {columns.map((column, colIndex) => (
+            <ul key={colIndex} className="w-1/3">
+              {column.map((link: FooterLink, index: number) => (
+                <li key={index} className="py-1">
+                  <a href={link.url} className="text-white hover:text-gray-400">
+                    {link.text}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          ))}
+        </div>
+        
+      </div>
+      <hr className="border-t-2 border-gray-300 my-4" />
+      <div className="container mx-auto flex justify-between items-center mt-4">
+        <span className="text-white text-sm font-medium">© 2024 Cursus. All Rights Reserved.</span>
+        <ul className="flex">
           {socialMediaLinksData.socialMedia.map((link: SocialMediaLink, index: number) => (
             <li key={index} className="mx-2">
               <a href={link.url} className="text-gray-600 hover:text-gray-900 text-xl">
@@ -30,8 +59,7 @@ const Footer: React.FC = () => {
             </li>
           ))}
         </ul>
-
-
+      </div>
     </footer>
   );
 };
--- src/routers/AppRouters.tsx
+++ src/routers/AppRouters.tsx
@@ -13,7 +13,18 @@ import CreateNewCousre from '../components/CreateNewCousre';
 import InstructorLayout from "../layouts/InstructorLayout";
 import ListCourse from "../page/Instructor/ListCourse";
 import Dashboard from "../page/Instructor/Dashboard";
+import Analyics from "../page/Instructor/Analyics";
+import Messages from "../page/Instructor/Messages";
+import Notifications from "../page/Instructor/Notifications";
+import MyCertificate from "../page/Instructor/MyCertificate";
+import CreateCourse from "../page/Instructor/CreateCourse";
+import Reviews from "../page/Instructor/Reviews";
+import Earning from "../page/Instructor/Earning";
+import PayOut from "../page/Instructor/PayOut";
+import Statements from "../page/Instructor/Statements";
 import SettingsPage from "../components/SettingsPage";
+
+
 const AppRouters: React.FC = () => {
   return (
     <Router>
@@ -27,8 +38,17 @@ const AppRouters: React.FC = () => {
         <Route path="/student_purchased_courses" element={<ProtectedRoute allowedRoles={[2]}><PurchasedCourses /></ProtectedRoute>} />
         <Route path="/student_my_certificates" element={<ProtectedRoute allowedRoles={[2]}><StudentCertificates /></ProtectedRoute>} />
         <Route path="/create-course" element={<ProtectedRoute allowedRoles={[3]}><CreateNewCousre /></ProtectedRoute>} />
-        <Route path="/instructorhome/list-courses" element={<InstructorLayout><ListCourse /></InstructorLayout>} />
-        <Route path="/instructorhome" element={<InstructorLayout><Dashboard /></InstructorLayout>} />
+        <Route path="/instructor_courses" element={<InstructorLayout><ListCourse /></InstructorLayout>} />
+        <Route path="/instructor_dashboard" element={<InstructorLayout><Dashboard /></InstructorLayout>} />
+        <Route path="/instructor_analyics" element={<InstructorLayout><Analyics /></InstructorLayout>} />
+        <Route path="/instructor_create_course" element={<InstructorLayout><CreateCourse /></InstructorLayout>} />
+        <Route path="/instructor_messages" element={<InstructorLayout><Messages /></InstructorLayout>} />
+        <Route path="/instructor_notifications" element={<InstructorLayout><Notifications /></InstructorLayout>} />
+        <Route path="/instructor_my_certificates" element={<InstructorLayout><MyCertificate /></InstructorLayout>} />
+        <Route path="/instructor_reviews" element={<InstructorLayout><Reviews /></InstructorLayout>} />
+        <Route path="/instructor_earning" element={<InstructorLayout><Earning /></InstructorLayout>} />
+        <Route path="/instructor_payout" element={<InstructorLayout><PayOut /></InstructorLayout>} />
+        <Route path="/instructor_statements" element={<InstructorLayout><Statements /></InstructorLayout>} />
         <Route path="/setting" element={<ProtectedRoute allowedRoles={[2]}><SettingsPage /></ProtectedRoute>} />
       </Routes>
     </Router>
