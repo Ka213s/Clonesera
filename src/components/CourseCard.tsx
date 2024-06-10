031474d844f73750e24025cef3e4bb8f96626b78
Ngocbao22 <107461196+Ngocbao22@users.noreply.github.com>
2024-06-10 15:07:29 +0700

    CSS

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -13,14 +13,9 @@ const CourseCard: React.FC<CourseCardProps> = ({ name, views, date, description,
   return (
     <div className="bg-gray-100 w-full gap-6 flex-wrap justify-center items-center">
       <div className='w-full p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl mt-4 mb-4 lg:mt-0'>
-        <video controls className="w-full h-40 object-cover rounded-t-lg mb-4">
-          <source src={require('../assets/30 Second Timer.mp4')} type="video/mp4" />
-          {/* Your browser does not support the video tag. */}
-        </video>
+      <img src={require('../assets/Course.png')} alt="Your Image" className="w-full h-full object-cover rounded-t-lg mb-4" />
         <h2 className="text-xl font-bold text-gray-900 truncate">{name}</h2>
         <p className="text-sm text-gray-600 mt-2"><strong>Views:</strong> {views}</p>
-        <p className="text-sm text-gray-600"><strong>Date:</strong> {date}</p>
-        <p className="text-sm text-gray-600 mt-4 truncate"><strong>Description:</strong> {description}</p>
         <p className="text-sm text-gray-600 mt-2"><strong>Author:</strong> {author}</p>
         <div className="mt-4">
         <p className="text-lg font-semibold text-gray-900"><strong>Price:</strong> ${price.toFixed(2)}</p>
