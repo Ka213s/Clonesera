cc3daf20e232af5f4c8353ddfce27438344c427e
Ngocbao22 <107461196+Ngocbao22@users.noreply.github.com>
2024-06-10 11:03:03 +0700

    CSS

--- src/components/Sidebar/Instructor_Sidebar.tsx
+++ src/components/Sidebar/Instructor_Sidebar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import instructorSidebar from '../../models/FileJson/instructorSidebar.json';
 import { FaHome, FaBook, FaChartLine, FaPlus, FaComments, FaBell, FaCertificate, FaStar, FaDollarSign, FaWallet, FaFile, FaCheck, FaCogs, FaPaperPlane } from 'react-icons/fa';
 
@@ -30,14 +31,16 @@ const iconComponents = {
 };
 
 const Sidebar: React.FC<SidebarProps> = ({ showMenu }) => {
+  const location = useLocation();
+
   return (
-    <aside className={`fixed top-16 left-0 h-full bg-white shadow-md transition-all duration-300 ${showMenu ? 'w-64' : 'w-0 overflow-hidden'}`}>
-      <ul className="mt-8">
+    <aside className={`fixed top-10 left-0 h-full bg-white shadow-md transition-all duration-300 ${showMenu ? 'w-64' : 'w-0 overflow-hidden'}`}>
+      <ul className="mt-8 max-h-full overflow-y-auto">
         {instructorSidebar.insSidebarItem.map((item: MenuItem, index: number) => (
-          <li key={index} className="flex items-center p-4 hover:bg-gray-200">
-            <a href={item.url} className="flex items-center space-x-3 text-gray-700">
+          <li key={index} className={`flex items-center p-4 ${location.pathname === item.url ? 'bg-[#8886E5]' : 'hover:bg-[#9997F5]'}`}>
+            <a href={item.url} className={`flex items-center space-x-3 text-gray-700 ${location.pathname === item.url ? 'text-white' : ''}`}>
               {iconComponents[item.icon as keyof typeof iconComponents]}
-              <span>{item.text}</span>
+               <span className={`${location.pathname === item.url ? 'text-white' : ''}`}>{item.text}</span>
             </a>
           </li>
         ))}
