15f59ca842aa17edd6b8e1068a38156b601eeef0
hfgkjyk <anhntdse160321@fpt.edu.vn>
2024-06-12 11:16:35 +0700

    add test page

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom'; // Import useLocation from react-router-dom
+import { useLocation } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import Sidebar from '../components/Sidebar/Sidebar';
@@ -7,9 +7,9 @@ import SidebarStudent from '../components/Sidebar/Student_Sidebar';
 import SidebarInstructor from '../components/Sidebar/Instructor_Sidebar';
 
 const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [showMenu, setShowMenu] = useState(true); // Initialize to true to show the sidebar by default
+  const [showMenu, setShowMenu] = useState(true);
   const [roleId, setRoleId] = useState<number | null>(null);
-  const location = useLocation(); // Get the current location using useLocation
+  const location = useLocation();
 
   useEffect(() => {
     const userData = localStorage.getItem('userData');
@@ -20,18 +20,16 @@ const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   }, []);
 
   useEffect(() => {
-    // Check if the current path is "/home" and always show the sidebar in that case
     if (location.pathname === '/home') {
       setShowMenu(true);
     }
-  }, [location.pathname]); // Listen for changes in the pathname
+  }, [location.pathname]);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
 
   const renderSidebar = () => {
-    // Check if the current path is "/home" or if roleId is not set, then always show the default Sidebar
     if (location.pathname === '/home' || roleId === null) {
       return <Sidebar showMenu={showMenu} />;
     }
@@ -47,7 +45,7 @@ const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   };
 
   return (
-    <div className="flex overflow-hidden"> 
+    <div className="flex overflow-hidden">
       <Header toggleMenu={toggleMenu} />
       {renderSidebar()}
       <div className={`flex flex-col flex-1 transition-all duration-300 ${showMenu ? 'ml-64' : 'ml-0'}`}>
