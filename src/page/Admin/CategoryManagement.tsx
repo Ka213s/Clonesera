3695838353487bdb661e590d3452fc3ad2cc4a5e
Ngocbao22 <107461196+Ngocbao22@users.noreply.github.com>
2024-07-01 16:39:46 +0700

    Update CSS

--- src/page/Admin/CategoryManagement.tsx
+++ src/page/Admin/CategoryManagement.tsx
@@ -79,6 +79,7 @@ const CategoryManagement: React.FC = () => {
             type="primary"
             icon={<PlusOutlined />}
             onClick={() => setIsModalVisible(true)}
+            className='!bg-[#9997F5] hover:!bg-[#8886E5]'
           >
             Add New Category
           </Button>
