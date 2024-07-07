822145c8836382c5c42de057dde026c1c4c05edd
Ngocbao22 <107461196+Ngocbao22@users.noreply.github.com>
2024-07-07 16:16:14 +0700

    Update CSS

--- src/page/VerifyEmailDone.tsx
+++ src/page/VerifyEmailDone.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { message, Typography } from 'antd';
 import { createApiInstance } from '../services/Api';
-import { SmileOutlined } from '@ant-design/icons';
+import { ReadOutlined } from '@ant-design/icons';
 
 const { Paragraph } = Typography;
 
@@ -16,7 +16,7 @@ function VerifyEmailDone() {
   const [countdown, setCountdown] = useState(5);
   const navigate = useNavigate();
   const api = createApiInstance(navigate);
-  const [isVerified, setIsVerified] = useState(false); // New state to control verification
+  const [isVerified, setIsVerified] = useState(false);
 
   useEffect(() => {
     const verifyEmail = async () => {
@@ -27,7 +27,7 @@ function VerifyEmailDone() {
       try {
         const result = await api.verifyEmail(token);
         setVerificationResult(result);
-        setIsVerified(true); // Set verification flag
+        setIsVerified(true);
         message.success('Email verification successful');
       } catch (error) {
         message.error('Error verifying email');
@@ -35,7 +35,7 @@ function VerifyEmailDone() {
     };
 
     verifyEmail();
-  }, [token, api, isVerified]); // Include isVerified in dependencies
+  }, [token, api, isVerified]);
 
   useEffect(() => {
     if (verificationResult) {
@@ -55,17 +55,17 @@ function VerifyEmailDone() {
   }, [verificationResult, navigate]);
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-50 animate-fadeIn">
-      <div className="flex flex-col items-center p-8 bg-white rounded-xl shadow-lg">
-        <SmileOutlined className="text-6xl text-green-500 mb-6" />
-        <Typography.Title level={2} className="mb-4 text-2xl font-bold text-[#6C6EDD]">
-          Email Verified
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-100 to-white">
+      <div className="flex flex-col items-center p-10 bg-white rounded-3xl shadow-2xl space-y-8 transition-all duration-500 ease-in-out transform hover:scale-105">
+        <ReadOutlined className="text-7xl text-blue-600 mb-6" />
+        <Typography.Title level={2} className="text-4xl font-extrabold text-blue-600">
+          Email Verified!
         </Typography.Title>
-        <Paragraph className="mb-2 text-lg text-[#4A4DC3]">
+        <Paragraph className="text-xl text-blue-500">
           Your email has been successfully verified.
         </Paragraph>
         {verificationResult && (
-          <Paragraph className="mt-4 text-base text-[#4A4DC3] animate-blink">
+          <Paragraph className="text-lg text-blue-400 animate-pulse">
             Redirecting to login page in {countdown} seconds...
           </Paragraph>
         )}
