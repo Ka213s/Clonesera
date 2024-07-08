5c8903f60fb259167014ad2ebdb3f757e1872636
kaka <113968332+Ka213s@users.noreply.github.com>
2024-07-09 06:02:35 +0700

    test deploy 2

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,7 +1,23 @@
-const test = () => {
-    return (
-      <div>testsasssssss</div>
-    )
-  }
-  
-  export default test
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Test = () => {
+  const [fact, setFact] = useState<string>('');
+
+  useEffect(() => {
+    axios.get('https://catfact.ninja/fact')
+      .then(response => {
+        console.log('Fetched data:', response.data);
+        setFact(response.data.fact);
+      })
+      .catch(error => {
+        console.error('Error fetching the cat fact:', error);
+      });
+  }, []);
+
+  return (
+    <div>{fact ? fact : 'Loading...'}</div>
+  );
+}
+
+export default Test;
