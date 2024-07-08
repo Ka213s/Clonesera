2337ec43d71f9d2cc45db2060d2d53e41ba001ee
kaka <113968332+Ka213s@users.noreply.github.com>
2024-07-09 06:45:13 +0700

    test deploy 10

--- src/router/AppRoutet.tsx
+++ src/router/AppRoutet.tsx
@@ -1,14 +1,28 @@
-import React, { lazy, Suspense } from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LayoutRoute from '../layout/LayoutRoute'; 
 
 const Test = lazy(() => import('../pages/test'));
+const AnotherPage = lazy(() => import('../pages/AnotherPage'));
+const Login = lazy(() => import('../pages/Login'));
+const CoursesTable = lazy(() => import('../pages/CoursesTable'));
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Suspense fallback={<div>Loading...</div>}> <Test /> </Suspense>} />
-      </Routes>
+      <Suspense >
+        <Routes>
+          {/* Routes with MainLayout */}
+          <Route element={<LayoutRoute />}>
+            <Route path="/test" element={<Test />} />
+          </Route>
+
+          {/* Routes without MainLayout */}
+          <Route path="/another-page" element={<AnotherPage />} />
+          <Route path="/courses" element={<CoursesTable />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
