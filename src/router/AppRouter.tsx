fd29cb1887fb31a825a51edf8acf70513fb41640
kaka <113968332+Ka213s@users.noreply.github.com>
2024-07-12 09:25:25 +0700

    update register

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,49 +1,53 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LayoutRoute from '../layout/LayoutRoute';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PrivateRoute from './PrivateRoute';
+import { ROLES, PATHS } from '../utils/commonImports';
 
-// Import Public Pages and Components directly
-import AnotherPage from '../pages/AnotherPage';
-import HomePage from '../pages/HomePage';
-import Login from '../pages/Login';
-import Logout from '../components/Logout';
-import Register from '../pages/Register';
-import Category from '../pages/Category';
-import SettingPage from '../pages/SettingPage';
-import Course from '../pages/Instructor/CreateCourse';
-import VerifyEmail from '../components/VerifyEmailDone';
+// Public Pages and Components
+const AnotherPage = lazy(() => import('../pages/AnotherPage'));
+const HomePage = lazy(() => import('../pages/HomePage'));
+const Login = lazy(() => import('../pages/Login'));
+const Logout = lazy(() => import('../components/Logout'));
+const Register = lazy(() => import('../pages/Register'));
+const Category = lazy(() => import('../pages/Category'));
+const SettingPage = lazy(() => import('../pages/SettingPage'));
+const Course = lazy(() => import('../pages/Instructor/CreateCourse'));
+const VerifyEmail = lazy(() => import('../components/VerifyEmailDone'));
 
-import PageError404 from '../pages/Error/PageError404';
-import PageError500 from '../pages/Error/PageError500';
-import PageError403 from '../pages/Error/PageError403';
+const PageError404 = lazy(() => import('../pages/Error/PageError404'));
+const PageError500 = lazy(() => import('../pages/Error/PageError500'));
+const PageError403 = lazy(() => import('../pages/Error/PageError403'));
 
 const AppRouter: React.FC = () => {
     return (
         <Router>
-            <Routes>
-                {/* Routes with MainLayout */}
-                <Route element={<LayoutRoute />}>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/logout" element={<Logout />} />
-                    <Route path="/courses" element={<Course />} />
-                    <Route path="/categories" element={<Category />} />
-                    <Route path="/setting-page" element={<SettingPage />} />
-                    <Route path="/500" element={<PrivateRoute element={PageError500} allowedRoles={['student']} />} />
-                </Route>
+            <Suspense>
+                <Routes>
+                    {/* Routes with MainLayout */}
+                    <Route element={<LayoutRoute />}>
+                        <Route path={PATHS.HOME} element={<HomePage />} />
+                        <Route path={PATHS.LOGOUT} element={<Logout />} />
+                        <Route path={PATHS.COURSE} element={<Course />} />
+                        <Route path={PATHS.CATEGORY} element={<Category />} />
+                        <Route path={PATHS.SETTING_PAGE} element={<SettingPage />} />
+                        <Route path={PATHS.ERROR500} element={<PrivateRoute element={PageError500} allowedRoles={[ROLES.STUDENT]} />} />
 
-                {/* Routes without MainLayout */}
-                {/* Public Routes */}
-                <Route path="/403" element={<PageError403 />} />
-                <Route path="/404" element={<PageError404 />} />
-                <Route path="/500" element={<PageError500 />} />
-                <Route path="/another-page" element={<AnotherPage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/verify-email/:token" element={<VerifyEmail />} />
-            </Routes>
+                    </Route>
+
+                    {/* Routes without MainLayout */}
+                    {/* Public Routes */}
+                    <Route path={PATHS.ERROR403} element={<PageError403 />} />
+                    <Route path={PATHS.ERROR404} element={<PageError404 />} />
+                    <Route path={PATHS.ERROR500} element={<PageError500 />} />
+                    <Route path={PATHS.ANOTHER_PAGE} element={<AnotherPage />} />
+                    <Route path={PATHS.LOGIN} element={<Login />} />
+                    <Route path={PATHS.REGISTER} element={<Register />} />
+                    <Route path={PATHS.VERIFY_EMAIL} element={<VerifyEmail />} />
+                </Routes>
+            </Suspense>
             <ToastContainer />
         </Router>
     );
